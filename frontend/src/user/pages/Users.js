ad0a700134b551a15e05930a3a333fa652933142
Bryan C Guner <bryan.guner@gmail.com>
2023-04-09 13:40:49 -0400

    Adding a UserList Page and component

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import UsersList from "../components/UsersList";
 const Users = () => {
-  return <h2>Users Page</h2>;
-};  
+  const USERS = [
+    {
+      id: "u1",
+      name: "Bryan Guner",
+      image:
+        "https://d33wubrfki0l68.cloudfront.net/e5828552ff6b5743ed241d9c926e60eb925dde97/8dbbf/images/goals.jpg",
+      places: 3,
+      },
+      
+  ];
+  return <UsersList items={USERS} />;
+};
 
 export default Users;
