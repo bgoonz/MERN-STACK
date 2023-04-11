5cb1f9b30a96a1a54b9fc25de46a6459554013ee
Bryan C Guner <bryan.guner@gmail.com>
2023-04-11 13:23:59 -0400

    adding input validation on blur using touchedHandler

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 import "./NewPlace.css";
-
+/*VALIDATOR_REQUIRE() returns a validator configuration object*/
 const NewPlace = () => {
   return (
-    <form className="place-form">
-      <Input element="input" type="text" label="title" validators={[]} errorText="Please enter a valid title"></Input>
+      <form className="place-form">
+
+      <Input element="input" type="text" label="title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title"></Input>
     </form>
   );
 };
