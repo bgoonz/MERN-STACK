e2e2255b410bbf5f152d00b08882f90cb7282f84
Bryan C Guner <bryan.guner@gmail.com>
2023-04-20 11:52:52 -0400

    implementing http-hook in Auth.js

--- frontend/src/shared/hooks/http-hook.js
+++ frontend/src/shared/hooks/http-hook.js
@@ -20,16 +20,21 @@ export const useHttpClient = () => {
           headers,
           signal: httpAbortCtrl.signal,
         });
-        const responseData = await response.json();
+          const responseData = await response.json();
+          // We want to clear the abort controllers from the request that just completed.
+          activeHttpRequests.current = activeHttpRequests.current.filter(
+              ( reqCtrl ) => reqCtrl !== httpAbortCtrl
+            );
         if (!response.ok) {
           throw new Error(responseData.message);
         }
+        setIsLoading(false);
         return responseData;
       } catch (err) {
         setError(err.message);
+        setIsLoading(false);
+        throw err;
       }
-
-      setIsLoading(false);
     },
     []
   );
